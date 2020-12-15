d5d2659fb73e3f796aee6125515823b075777f0d
Valeriy Harlamov <valera545@gmail.com>
2020-12-15 23:23:07 +0300

    Сортировка массива строк
    Очень трудно было догадаться, как прописать смешанную локаль. Нигде этого нет, догадался методом тыка, по наитию.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let tmpArr = arr.slice();
+    if( param == 'asc' ) {
+        return tmpArr.sort( (a, b) => a.localeCompare( b, 'ru-en', { caseFirst:'upper', sensitivity:'case', usage:'sort' } ) );
+    } else  {
+        return tmpArr.sort( (a, b) => a.localeCompare( b, 'ru-en', { caseFirst:'lower', sensitivity:'case', usage:'sort' } ) ).reverse();
+    };
 }
+//console.log(sortStrings(['Абрикос', 'абрикос', 'Ёжик', 'ёжик', 'Яблоко', 'яблоко', 'apple', 'Apple', 'banana', 'Banana', 'orange', 'Orange'], 'desc'))
