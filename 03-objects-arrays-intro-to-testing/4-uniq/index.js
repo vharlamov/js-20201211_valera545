c8461afd5acb62a075f033a2683532168aa9b2f1
Valeriy Harlamov <valera545@gmail.com>
2020-12-23 15:38:28 +0300

    a

---
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -0,0 +1,9 @@
+/**
+ * uniq - returns array of uniq values:
+ * @param {*[]} arr - the array of primitive values
+ * @returns {*[]} - the new array with uniq values
+ */
+export function uniq(arr) {
+  const tmp = new Set( arr );
+  return [...tmp ];
+}
